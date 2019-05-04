5187cdfca39ee16c4006e007e3a5c5265497a2d2
Osmar Hernández <osmarhernandezs@gmail.com>
2019-05-04 00:04:09 -0600

    updated

--- src/main/java/com/pastdue/App.java
+++ src/main/java/com/pastdue/App.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class App
 {
 
-    @RequestMapping("/")
+    @RequestMapping("welcome")
     @ResponseBody
     public String welcome() {
         return "Welcome to Money Together API v1!";
