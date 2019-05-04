37d2106e207c5c8fe208faf710e0d9a28cb971c3
Osmar Hernández <osmarhernandezs@gmail.com>
2019-05-04 00:36:56 -0600

    Updated

--- src/main/java/com/pastdue/App.java
+++ src/main/java/com/pastdue/App.java
@@ -2,21 +2,10 @@ package com.pastdue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class App
 {
-
-    @RequestMapping("/welcome")
-    @ResponseBody
-    public String welcome() {
-        return "Welcome to Money Together API v1!";
-    }
-
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
